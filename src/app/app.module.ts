1f56f3b77e07ad4a579beb5c9ac5837b3d7abeb3
Евгений Войтенко <e.voitenko@mangotele.com>
2019-05-23 14:30:14 +0300

    two input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppComponent } from "./app.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FlexLayoutModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
